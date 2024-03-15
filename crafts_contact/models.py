6f52cc0edc915cddb62e1c0da6de9fdd94696cf1
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-15 13:38:36 +0100

    Add limitations on contact model

--- crafts_contact/models.py
+++ crafts_contact/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.core.mail import send_mail
 from django.conf import settings
+from django.core.validators import validate_email
 
 
 # Some help with choices field option was collected from https://stackoverflow.com/questions/18676156/how-to-properly-use-the-choices-field-option-in-django
 class Contact(models.Model):
     class ContactChoices(models.TextChoices):
-        REPORT = "1", "Report user"
-        BUSINESS = "2", "Business inquiries"
+        REPORT = "1", "Business inquiries"
+        BUSINESS = "2", "Report user"
         FEEDBACK = "3", "Feedback about website"
         OTHER = "4", "Other questions"
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    subject = models.CharField(max_length=100)
-    message = models.TextField()
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(validators=[validate_email], blank=False)
+    subject = models.CharField(max_length=100, blank=False)
+    message = models.TextField(default='', blank=False)
     ContactChoices = models.CharField(
         max_length=1,
         choices=ContactChoices.choices,
@@ -23,12 +24,11 @@ class Contact(models.Model):
     def __str__(self):
         return self.name + " - " + self.subject
     
-    def save(self, *args, **kwargs):
-        #Send confirmation email to user
+    def send_confirmation_email(self):
         subject = "Thank you for your email"
         message = "Thank you for contacting us. We will get back to you as soon as possible."
         sender_email = self.email
-        
+
         send_mail(
             subject=subject,
             message=message,
@@ -36,4 +36,6 @@ class Contact(models.Model):
             recipient_list=[settings.EMAIL_HOST_USER],
         )
 
+    def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+        self.send_confirmation_email()
