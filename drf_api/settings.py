2764060570bcc18a0161a41fc3ff393b4bb210d3
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-17 12:22:21 +0100

    Prepare for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -67,12 +67,12 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['world-of-craft.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost', 'world-of-craft.herokuapp.com']
 
 
 # Application definition
