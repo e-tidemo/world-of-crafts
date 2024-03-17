9704de7128410fcdda0eee77db9f70fda85d2189
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-17 12:17:59 +0100

    Change DATABASE_URL

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -57,6 +57,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZERS': 'drf_api.serializers.CurrentUserSerializer'
@@ -128,8 +129,6 @@ else:
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'drf_api.urls'
-
-JWT_AUTH_SAMESITE = 'None'
 
 TEMPLATES = [
     {
