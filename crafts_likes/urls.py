312adae5504b7bcb32bd5a83f98b2ae4b302124d
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-25 13:34:29 +0100

    Change urls in likes and migrate changes

--- crafts_likes/urls.py
+++ crafts_likes/urls.py
@@ -3,5 +3,5 @@ from crafts_likes import views
 
 urlpatterns = [
     path('likes/', views.LikesList.as_view()),
-    path('likes/<int:pk>', views.like,name='like'),
+    path('likes/<int:pk>/', views.LikesDetail.as_view()),
 ]
