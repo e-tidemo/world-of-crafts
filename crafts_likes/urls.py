d70d95bfe22530680baa672d200428bec9c1fb7a
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-25 13:30:30 +0100

    change letter in urls.py for likes

--- crafts_likes/urls.py
+++ crafts_likes/urls.py
@@ -3,6 +3,5 @@ from crafts_likes import views
 
 urlpatterns = [
     path('likes/', views.LikesList.as_view()),
-    path('like/<int:post_id>', views.like,name='like'),
-    path('unlike/<int:post_id>', views.unlike,name='unlike')
+    path('likes/<int:pk>', views.like,name='like'),
 ]
